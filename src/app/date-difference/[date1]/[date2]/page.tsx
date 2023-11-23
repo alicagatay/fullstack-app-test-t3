0f758143c72694cef65df6cb2d0a0e2bb8fe3fd5
Ali Cagatay <aliccagatay@gmail.com>
2023-11-23 14:36:42 +0000

    Add date difference calculation and display the date difference in days.

--- src/app/date-difference/[date1]/[date2]/page.tsx
+++ src/app/date-difference/[date1]/[date2]/page.tsx
@@ -4,10 +4,23 @@ import { useParams } from "next/navigation";
 import styles from "./styles.module.css";
 export default function Page() {
   const params = useParams();
+  // convert date1 and date2 to Date objects
+  //the date format is YYYY-MM-DD in a string format
+  //then calculate the difference between the two dates
+  //and display the difference in days
+
+  const date1 = new Date(params.date1 as string);
+  const date2 = new Date(params.date2 as string);
+
+  const difference = date2.getTime() - date1.getTime();
+
+  const days = difference / (1000 * 3600 * 24);
+
   return (
     <div className={styles.main}>
       <h1>Date 1: {params.date1}</h1>
       <h1>Date 2: {params.date2}</h1>
+      <h1>Difference in days: {days}</h1>
     </div>
   );
 }
