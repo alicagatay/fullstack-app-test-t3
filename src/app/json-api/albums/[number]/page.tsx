169e65f62ab4d65c588794d965ad3f478c334ac1
Ali Cagatay <aliccagatay@gmail.com>
2023-11-23 16:39:35 +0000

    Update imports.

--- src/app/json-api/albums/[number]/page.tsx
+++ src/app/json-api/albums/[number]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import { useParams } from "next/navigation";
 
